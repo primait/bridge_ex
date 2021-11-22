230b18aa90e751158ca071b21e51533a751100ee
Matteo Busi <mbusi81@gmail.com>
2021-11-22 10:52:27 +0100

    Bump version to 0.1.1

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule BridgeEx.MixProject do
   def project do
     [
       app: :bridge_ex,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.10",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
