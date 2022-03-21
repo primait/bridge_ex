f662c59f772946e36739478662d2b49bd53f0610
Cristiano Piemontese <cristiano.piemontese@prima.it>
2022-03-21 10:55:37 +0100

    add more tests

--- test/graphql/retry_test.exs
+++ test/graphql/retry_test.exs
@@ -10,6 +10,28 @@ defmodule BridgeEx.Graphql.RetryTest do
     {:ok, counter: counter}
   end
 
+  test "returns function result without retrying on success", %{counter: counter} do
+    expected_result = {:ok, "Some result"}
+
+    mock_function = fn _ ->
+      Counter.increment(counter)
+      expected_result
+    end
+
+    max_retries = 3
+
+    result =
+      Retry.retry(
+        "Some argument",
+        mock_function,
+        &retry_always/1,
+        max_retries
+      )
+
+    assert Counter.count(counter) == 1
+    assert result == expected_result
+  end
+
   test "retry a function on error until max retries", %{counter: counter} do
     mock_function = fn _ ->
       Counter.increment(counter)
@@ -19,13 +41,34 @@ defmodule BridgeEx.Graphql.RetryTest do
     expected_retries = 3
 
     Retry.retry(
-      {:ok, ""},
+      "Some argument",
       mock_function,
       &retry_always/1,
       expected_retries
     )
 
     assert Counter.count(counter) == expected_retries
+  end
+
+  test "retry a function based on policy", %{counter: counter} do
+    mock_function = fn _ ->
+      Counter.increment(counter)
+      {:error, "Error"}
+    end
+
+    max_retries = 3
+
+    Retry.retry(
+      "Some argument",
+      mock_function,
+      fn
+        "Error" -> false
+        _ -> true
+      end,
+      max_retries
+    )
+
+    assert Counter.count(counter) == 1
   end
 
   def retry_always(_), do: true
