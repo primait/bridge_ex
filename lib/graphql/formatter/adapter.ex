87cd82997e86c7ea78564233019fcfb987947555
Cristian <cristian.montini@prima.it>
2022-07-04 13:39:52 +0200

    add custom formatter behaviour

---
+++ lib/graphql/formatter/adapter.ex
@@ -0,0 +1,30 @@
+defmodule BridgeEx.Graphql.Formatter.Adapter do
+  @moduledoc """
+  Behaviour used to implement a formatter adapter which could be used to
+  format types for both input variables and responses.
+
+  ```elixir
+  defmodule MyCustomAdapter do
+
+    @behaviour BridgeEx.Graphql.Formatter.Adapter
+
+    def format(variable) when is_map(variable) do
+      variable
+      |> Enum.map(fn
+        {key, value} -> {key, format(value)}
+      end)
+      |> Map.new()
+    end
+
+    def format(variable = %Date{}), do: Date.to_string(variable)
+  end
+
+  defmodule MyBridge do
+    use BridgeEx.Graphql,
+      variable_types_formatter: MyCustomAdapter
+  end
+  ```
+  """
+
+  @callback format(any()) :: any()
+end
