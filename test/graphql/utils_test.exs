2b4092fe73d60b57ff025ae6b9c74028b38a486b
Fabio Di Peri <fabio.diperi@prima.it>
2026-04-14 15:53:05 +0200

    Fix formatting issues

--- test/graphql/utils_test.exs
+++ test/graphql/utils_test.exs
@@ -5,17 +5,22 @@ defmodule BridgeEx.Graphql.UtilsTest do
 
   describe "parse_operation_metadata/1" do
     test "extracts type and name from a named query" do
-      assert {"query", "FindUser"} = Utils.parse_operation_metadata("query FindUser { user { id } }")
+      assert {"query", "FindUser"} =
+               Utils.parse_operation_metadata("query FindUser { user { id } }")
     end
 
     test "extracts type and name from a named mutation" do
       assert {"mutation", "CreateOrder"} =
-               Utils.parse_operation_metadata("mutation CreateOrder($input: OrderInput!) { createOrder(input: $input) { id } }")
+               Utils.parse_operation_metadata(
+                 "mutation CreateOrder($input: OrderInput!) { createOrder(input: $input) { id } }"
+               )
     end
 
     test "extracts type and name from a named subscription" do
       assert {"subscription", "OnOrderUpdated"} =
-               Utils.parse_operation_metadata("subscription OnOrderUpdated { orderUpdated { id } }")
+               Utils.parse_operation_metadata(
+                 "subscription OnOrderUpdated { orderUpdated { id } }"
+               )
     end
 
     test "returns anonymous for a named-less query keyword" do
@@ -23,7 +28,8 @@ defmodule BridgeEx.Graphql.UtilsTest do
     end
 
     test "returns anonymous for a named-less mutation keyword" do
-      assert {"mutation", "anonymous"} = Utils.parse_operation_metadata("mutation { createUser { id } }")
+      assert {"mutation", "anonymous"} =
+               Utils.parse_operation_metadata("mutation { createUser { id } }")
     end
 
     test "returns query/anonymous for shorthand query (no keyword)" do
